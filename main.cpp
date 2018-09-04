5e8bfff549daddb66618f6aaa8ae22b3a7006783
Giovanni Lucaroni <Giovalucaroni2@gmail.com>
2018-09-04 16:45:57 +0200

    non so ma dopo tante madonna ora va

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include <gdal_priv.h>
+#include <cpl_conv.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()  {
+        const char *input = "/home/giovanni/Desktop/expected.tif";
+        const char *output = "/home/giovanni/Desktop/result.png";
+
+        GDALAllRegister();
+
+        GDALDataset  *oldDataSet;
+        GDALDriver *outputPNG;
+        outputPNG = GetGDALDriverManager()->GetDriverByName("PNG");
+
+        oldDataSet  = (GDALDataset *) GDALOpen(input, GA_ReadOnly );
+
+        std::cout << "ora va cazzo";
+
     return 0;
 }
