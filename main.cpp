c473b70d89ecf7e96f88db03cd2eca5d33550d35
Giovanni Lucaroni <giovalucaroni2@gmail.com>
2018-09-21 12:27:24 +0200

    Speed up
    Signed-off-by: Giovanni Lucaroni <Giovalucaroni2@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <gdal_priv.h>
 #include <filesystem>
 #include <gdal_utils.h>
+#include <iostream>
+#include <ImageMagick-7/Magick++.h>
+#include <omp.h>
 
 /**
  * Generate colored, upscaled, temporized map, with the Italian alert zones
@@ -15,9 +18,9 @@ using namespace std::chrono;
 
 static std::string DIR_FORMAT = "%Y%m%d_%H";
 static std::string DATE_FORMAT = "%Y/%m/%d H%H UTF-0 ";
-static std::string TEMP_PATH = "/mnt/ramdisk/TEMP/";
+static std::string TEMP_PATH = "TEMP/";
 static std::string PREVISTE = "/cf_psm.tif";
-static std::string BASE_PATH = "/home/giovanni/Desktop/dati/";
+static std::string BASE_PATH = "dati/";
 static std::string COLORI = "colors.txt";
 
 /**
@@ -44,8 +47,6 @@ void to3857(double *x, double *y) {
 }
 
 int main(int argc, char *argv[]) {
-    //timing for bench
-//    high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
     char dirName[12];
     char timestamp[22];
@@ -64,67 +65,55 @@ int main(int argc, char *argv[]) {
     char *optionForDEM[] = {const_cast<char *>("-alpha"), nullptr};
     GDALDEMProcessingOptions *options = GDALDEMProcessingOptionsNew(optionForDEM, nullptr);
 
-    //read the background and the "alert zones" (za)
-//        Magick::Image background;
-//        Magick::Image za;
-//        background.read("/home/giovanni/CLionProjects/MappeIRPI-CNR/sfondo2.mpc");
-//        za.read("/home/giovanni/CLionProjects/MappeIRPI-CNR/ZA.mpc");
-    //create a vector for create the gif
-    //i suspect that this method is really slow
-//        std::vector<Magick::Image> frames;
+    Magick::Image background;
+    Magick::Image za;
+    background.read(TEMP_PATH + "background.mpc");
+    za.read(TEMP_PATH + "/za.mpc");
 
-
-    int g;
     time_t date;
-    int i;
+    int g;
 
-    for (i = 0; i < diffHours; ++i) {
-        //start of gdal processing block
+    startDate = toTime(std::stringstream(argv[1]));
+    for (int j = 0; j < diffHours; ++j) {
         date = timegm(&startDate);
         strftime(dirName, 12, DIR_FORMAT.c_str(), gmtime(&date));
 //        fs::create_directory(fs::path(TEMP_PATH + dirName));
 
         originalDataset = (GDALDataset *) GDALOpen((BASE_PATH + dirName + PREVISTE).c_str(), GA_ReadOnly);
-        newDataset = (GDALDataset *) GDALDEMProcessing((TEMP_PATH + std::to_string(i) + "cp.tif").c_str(),
+        newDataset = (GDALDataset *) GDALDEMProcessing((TEMP_PATH + std::to_string(j) + "cf_psm.tif").c_str(),
                                                        originalDataset,
                                                        "color-relief",
                                                        COLORI.c_str(), options, &g);
         GDALClose(newDataset); //write the processed tif to disk
-
-        //start of the Magick++ block
-//            Magick::Image tif;
-        //read the block
-//            tif.read(TEMP_PATH + dirName + PREVISTE);
-//            tif.scale(Magick::Geometry(1083, 1166));
-        //add the background and the za
-        //i want to apply that to the final gif, not to every single photo
-//            tif.composite(background, 0, 0, Magick::DstOverCompositeOp);
-//            tif.composite(za, 0, 0, Magick::OverCompositeOp);
-        //options for annotate the frame
-//            tif.font("/usr/share/fonts/OTF/SFMono-Bold.otf");
-//            tif.fillColor("White");
-//            tif.fontPointsize(37);
-//            tif.boxColor("Black");
-//            strftime(timestamp, 22, DATE_FORMAT.c_str(), gmtime(&date));
-//            tif.annotate(timestamp, Magick::NorthEastGravity);
-        //add the frame to the vector add set the animation delay
-//        tif.magick("TIFF");
-//        tif.quality(7);
-//            frames.push_back(tif);
-//            tif.animationDelay(3000);
-
         startDate.tm_hour += 1;
     }
 
-    //write the gif to disk, this takes a very long time
-//        Magick::writeImages(frames.begin(), frames.end(), TEMP_PATH + "sss.gif");
+    startDate = toTime(std::stringstream(argv[1]));
+#pragma omp ordered
+    for (int i = 0; i < diffHours; ++i) {
+        date = timegm(&startDate);
+        //start of gdal processing block
+        Magick::Image tif;
+        tif.read(std::to_string(i) + "cf_psm.tif");
+        tif.scale(Magick::Geometry(1082, 1166));
+//        add the background and the za
+//        i want to apply that to the final gif, not to every single photo
+        tif.composite(background, 0, 0, Magick::DstOverCompositeOp);
+        tif.composite(za, 0, 0, Magick::OverCompositeOp);
+        //options for annotate the frame
+        tif.font("/usr/share/fonts/OTF/SFMono-Bold.otf");
+        tif.fillColor("White");
+        tif.fontPointsize(37);
+
+        strftime(timestamp, 22, DATE_FORMAT.c_str(), gmtime(&date));
+        tif.annotate(timestamp, Magick::NorthEastGravity);
+        tif.write(TEMP_PATH + std::to_string(i) + "cf_psm.tif");
+        startDate.tm_hour += 1;
+    }
 
     GDALClose(originalDataset);
     GDALDEMProcessingOptionsFree(options);
 
-//    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-//    auto duration = duration_cast<milliseconds>(t2 - t1).count();
-//    std::cout << duration;
 
     return 0;
 }
